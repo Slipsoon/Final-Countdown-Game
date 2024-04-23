6dcca78bafc150fadcc0904ffeb725c533b52981
Sebastian Ślipek <sebastian.slipek@hotmail.com>
2024-04-23 15:57:31 +0200

    Project Init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
+
+function App() {
+  return (
+    <>
+      <Player />
+      <div id="challenges"></div>
+    </>
+  );
+}
+
+export default App;
